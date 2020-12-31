069efb4c3d7ecd2615150de9474fe821159bd3cc
Shruthik Musukula <shruthik.musukula@gmail.com>
2020-12-30 19:01:06 -0800

    Updated component.

--- part-one/src/index.js
+++ part-one/src/index.js
@@ -1,16 +1,23 @@
 import ReactDOM from 'react-dom'
 
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
+  const name = 'Peter'
+  const age = 10
 
   return (
     <div>
-      <p>Current date is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
